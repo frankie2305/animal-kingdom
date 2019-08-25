bd5cff0d4f0e81078c0e0528372500b2eb7ee389
frankie2305 <51467562+frankie2305@users.noreply.github.com>
2019-08-26 09:06:11 +1000

    Make the color of Grass more realistic

--- src/app/Grass.java
+++ src/app/Grass.java
@@ -9,7 +9,7 @@ class Grass extends Cell {
         super(x, y);
         Random rand = new Random();
         int r = rand.nextInt(28);
-        int g = rand.nextInt(256);
+        int g = rand.nextInt(156) + 100;
         int b = rand.nextInt(28);
         this.color = new Color(r, g, b);
         this.animal = "Rabbit";
